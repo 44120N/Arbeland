7bb7d6b86ca03547235bff0865ac460422c059ae
Benedict Halim <benedicthalim1@gmail.com>
2024-12-04 16:58:27 +0700

    Routing setup & boilerplate for ease in designing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Boilerplate from "./pages/Boilerplate.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/Arbeland/",
+    element: <Boilerplate />,
+    children: [
+      { path: "", element: <Boilerplate /> },
+      {
+        path: "boilerplate",
+        element: <Boilerplate />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/Boilerplate.jsx
@@ -0,0 +1,18 @@
+import { Stack } from "@mui/material";
+
+export default function Boilerplate() {
+  return (
+    <>
+      <Stack>
+        <Typography variant="h1">This is The Title</Typography>
+        <Typography variant="h2">This is The Title</Typography>
+        <Typography variant="h3">This is The Title</Typography>
+        <Typography variant="h4">This is The Title</Typography>
+        <Typography variant="h5">This is The Title</Typography>
+        <Typography variant="h6">This is The Title</Typography>
+        <Typography variant="p">lorem ipsum dolor sit amet</Typography>
+        <Typogrpahy variant=""></Typogrpahy>
+      </Stack>
+    </>
+  );
+}
