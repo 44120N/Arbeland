4f39051365fee8b313489b5f9a3540a0056d8d89
Benedict Halim <benedicthalim1@gmail.com>
2024-12-05 10:26:27 +0700

    boilerplate update

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import Boilerplate from "./pages/Boilerplate.jsx";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {
-    path: "/Arbeland/",
-    element: <Boilerplate />,
-    children: [
-      { path: "", element: <Boilerplate /> },
-      {
-        path: "boilerplate",
+    {
+        path: "/Arbeland/",
         element: <Boilerplate />,
-      },
-    ],
-  },
+        children: [
+            { path: "", element: <Boilerplate /> },
+            {
+                path: "boilerplate",
+                element: <Boilerplate />,
+            },
+        ],
+    },
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 );
--- src/pages/Boilerplate.jsx
+++ src/pages/Boilerplate.jsx
@@ -8,28 +8,15 @@ export default function Boilerplate() {
                 <Container size="xl">
                     <Stack gap={5}>
                         <Stack>
-                            <Typography variant="h1">
-                                This is The Title
-                            </Typography>
-                            <Typography variant="h2">
-                                This is The Title
-                            </Typography>
-                            <Typography variant="h3">
-                                This is The Title
-                            </Typography>
-                            <Typography variant="h4">
-                                This is The Title
-                            </Typography>
-                            <Typography variant="h5">
-                                This is The Title
-                            </Typography>
-                            <Typography variant="h6">
-                                This is The Title
-                            </Typography>
+                            <Typography variant="h1">This is h1</Typography>
+                            <Typography variant="h2">This is h2</Typography>
+                            <Typography variant="h3">This is h3</Typography>
+                            <Typography variant="h4">This is h4</Typography>
+                            <Typography variant="h5">This is h5</Typography>
+                            <Typography variant="h6">This is h6</Typography>
                             <Typography variant="p">
                                 lorem ipsum dolor sit amet
                             </Typography>
-                            <Typography variant=""></Typography>
                         </Stack>
                         <Stack>
                             <Button
