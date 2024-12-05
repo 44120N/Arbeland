dc47a62fdcf60319f74d91812bbecfe683480008
Benedict Halim <benedicthalim1@gmail.com>
2024-12-05 11:51:32 +0700

    routing and pages revamp

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Boilerplate from "./pages/Boilerplate.jsx";
 import App from "./App.jsx";
+import "./index.css";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/Arbeland/",
         element: <App />,
         children: [
-            { path: "", element: <Boilerplate /> },
+            { path: "", element: <Home /> },
             {
                 path: "boilerplate",
                 element: <Boilerplate />,
---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Stack } from "@mui/material";
+import Accordion from "../components/Accordion";
+import Button from "../components/Button";
+import Drawer from "../components/Drawer";
+
+export default function Home() {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const menuItems = [
+        {
+            label: "Home",
+            onClick: () => console.log("Home clicked"),
+            href: "/#",
+        },
+        {
+            label: "About",
+            onClick: () => console.log("About clicked"),
+            href: "/#",
+        },
+        {
+            label: "Contact",
+            onClick: () => console.log("Contact clicked"),
+            href: "/#",
+        },
+    ];
+    return (
+        <>
+            <Stack
+                justifyContent={"center"}
+                alignItems={"center"}
+                direction={"column"}
+            >
+                <div className="flex flex-col items-center justify-center min-h-screen bg-[#f3f3f3] dark:bg-[#121212]">
+                    <Accordion
+                        bgcolor_title={"#93c5fd"}
+                        color_title={"#fff"}
+                        bgcolor_text={"#fff"}
+                        color_text={"#000"}
+                        bdcolor={"#000"}
+                        question="What is Neobrutalism?"
+                        answer="Neobrutalism is a design style characterized by bold colors, strong contrasts, and a minimalistic approach, often with a 'raw' aesthetic."
+                    />
+                    <Button>Test</Button>
+                    <br />
+                    <Button onClick={() => setDrawerOpen(true)}>Sidebar</Button>
+                    <Drawer
+                        list={menuItems}
+                        title="Menu"
+                        active={drawerOpen}
+                        setActive={setDrawerOpen}
+                        bgcolor={"#93c5fd"}
+                        color={"#fff"}
+                        bdcolor={"#000"}
+                    />
+                </div>
+            </Stack>
+        </>
+    );
+}
