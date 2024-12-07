7ac89f7add6498ea2c95294678944d43961f8b3a
Benedict Halim <benedicthalim1@gmail.com>
2024-12-07 10:22:19 +0700

    Content addition in /about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,16 +10,65 @@ export default function About() {
                         <Typography variant="h1">About Us</Typography>
                         <Divider sx={{ borderBottom: "3px solid black" }} />
                     </Stack>
-                    <Stack>
-                        <Accordion
-                            bgcolor_title={"#93c5fd"}
-                            color_title={"#fff"}
-                            bgcolor_text={"#fff"}
-                            color_text={"#000"}
-                            bdcolor={"#000"}
-                            question="What does ARBELAND mean?"
-                            answer="ARBELAND is an acronym of the three founders: Aaron, Benedict, Roland"
-                        />
+                    <Stack
+                        direction={{ xs: "col", md: "row" }}
+                        sx={{
+                            justifyContent: "space-between",
+                            p: 3,
+                            color: "white.main",
+                        }}
+                        bgcolor="tertiary.main"
+                    >
+                        <Stack>
+                            <Typography variant={"h3"} sx={{ mb: { xs: 3 } }}>
+                                Frequently Asked Questions
+                            </Typography>
+                        </Stack>
+                        <Stack>
+                            <Accordion
+                                bgcolor_title={"#93c5fd"}
+                                color_title={"#fff"}
+                                bgcolor_text={"#fff"}
+                                color_text={"#000"}
+                                bdcolor={"#000"}
+                                question="What do we offer?"
+                                answer="ARBELAND provides courses in mathemathics and website development(frontend and/or backend)"
+                            />
+                            <Accordion
+                                bgcolor_title={"#93c5fd"}
+                                color_title={"#fff"}
+                                bgcolor_text={"#fff"}
+                                color_text={"#000"}
+                                bdcolor={"#000"}
+                                question="How much does it cost?"
+                                answer="Trial first, talk about the price later. So you won't lose any money."
+                            />
+                        </Stack>
+                    </Stack>
+                    <Stack
+                        direction={{ xs: "column-reverse", md: "row" }}
+                        bgcolor="tertiary.main"
+                        sx={{ p: 3, justifyContent: "space-between" }}
+                    >
+                        <Stack>
+                            <Accordion
+                                bgcolor_title={"#93c5fd"}
+                                color_title={"#fff"}
+                                bgcolor_text={"#fff"}
+                                color_text={"#000"}
+                                bdcolor={"#000"}
+                                question="What does ARBELAND mean?"
+                                answer="ARBELAND is an acronym of the three founders: Aaron, Benedict, Roland"
+                            />
+                        </Stack>
+                        <Stack>
+                            <Typography
+                                variant="h3"
+                                sx={{ color: "white.main", mb:{xs:3}}}
+                            >
+                                Trivia
+                            </Typography>
+                        </Stack>
                     </Stack>
                 </Stack>
             </Container>
